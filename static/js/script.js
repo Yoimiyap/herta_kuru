c9af090e5a950ac45e7e301563b5ca27d42b34b4
Yoimiyap <102911376+Yoimiyap@users.noreply.github.com>
2026-07-04 21:24:55 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,8 +3,9 @@ var LANGUAGES = {
     "en": {
         audioList: [
             // TODO audio random weight
-            "audio/cn/转圈圈.mp3",
-            "audio/cn/转圈圈咯.mp3"
+            "audio/en/en_1.mp3",
+            "audio/en/en_2.mp3",
+            "audio/en/en_3.mp3"
         ],
         texts: {
             "page-title": "Welcome to herta kuru~",
